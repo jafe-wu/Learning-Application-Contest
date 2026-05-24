f26a1df1fd81bc168b5aaf10e80fe70e8c7260fc
c88675877-jpg <c88675877@gmail.com>
2026-05-24 23:08:58 +0800

    优化构建流程并增强对多环境的兼容性
    新增对 Visual Studio 安装路径的自动检测逻辑，支持自定义路径。
    在 `CMakePresets.json` 中添加 `debug` 和 `release` 构建预设，使用 Ninja 生成器和 MSVC 编译器。
    更新 `run.bat` 脚本，调整为 UTF-8 编码，动态获取脚本目录，优化编译与运行流程，移除无关文件操作。
    在 `settings.local.json` 中新增 `cmake --version` 命令。
    
    整体改动提升了构建脚本的可读性、可维护性和环境适配能力。

--- run.bat
+++ run.bat
@@ -1,14 +1,21 @@
 @echo off
-cd /d "E:\User\Desktop\code_competition_verson\22\Learning-Application-Contest\code"
-call "E:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" x64 >nul 2>&1
-cmake --build ../build
+chcp 65001 >nul
+
+set _ROOT=%~dp0
+
+call "E:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat" >nul
+
+echo ====== 编译 ======
+cmake -S "%_ROOT%code" -B "%_ROOT%code\out\build\x64-Debug" -G Ninja -DCMAKE_BUILD_TYPE=Debug >nul
+cmake --build "%_ROOT%code\out\build\x64-Debug"
 if %errorlevel% neq 0 (
     echo.
     echo 编译失败！
     pause
     exit /b 1
 )
-copy /y dims.csv ..\build\ >nul 2>&1
-copy /y "包装来料数据.csv" ..\build\orders.tsv >nul 2>&1
-cd /d "E:\User\Desktop\code_competition_verson\22\Learning-Application-Contest\build"
+
+echo.
+echo ====== 运行 ======
+cd /d "%_ROOT%code\out\build\x64-Debug"
 main.exe
